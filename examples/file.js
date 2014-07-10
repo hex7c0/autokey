ef4f113b42c3595e2a2919169cb7d4174c1773f2
hex7c0 <carniellifrancesco@gmail.com>
2014-07-10 08:45:43 +0200

    file example

---
+++ examples/file.js
@@ -0,0 +1,55 @@
+"use strict";
+/**
+ * @file file example
+ * @module autokey
+ * @package autokey
+ * @subpackage examples
+ * @version 0.0.1
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+try {
+    var autokey = require('../index.js'); // use require('autokey') instead
+    var fs = require('fs');
+} catch (MODULE_NOT_FOUND) {
+    console.error(MODULE_NOT_FOUND);
+    process.exit(1);
+}
+
+var a = 'hex7c0'; // key
+var b = new Buffer('ciao I\'m hex7c0\nHow are you?\n:D'); // data
+var cipher = autokey(a);
+
+var d = cipher.encodeBuffer(b); // encrypt
+
+console.log('original: ' + b.toString());
+
+// use {encoding: null} when you write buffer
+fs.writeFile('crypted',d,{
+    encoding: null
+},function(err) {
+
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('encrypt: ' + d);
+    }
+});
+
+// use {encoding: null} when you read buffer
+fs.readFile('crypted',{
+    encoding: null
+},function(err,data) {
+
+    if (err) {
+        console.log(err);
+    } else {
+        var e = cipher.decodeBuffer(data); // decrypt
+        console.log('decrypt: ' + e.toString());
+    }
+});
