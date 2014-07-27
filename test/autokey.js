f62ee6365239e7274fdc4671592a36455f8fdcdb
hex7c0 <carniellifrancesco@gmail.com>
2014-07-27 17:10:05 +0200

    using mocha

---
+++ test/autokey.js
@@ -0,0 +1,85 @@
+"use strict";
+/**
+ * @file autokey test
+ * @module autokey
+ * @package autokey
+ * @subpackage test
+ * @version 0.0.1
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+try {
+    var autokey = require('../index.min.js'); // use require('autokey') instead
+    var assert = require('assert');
+} catch (MODULE_NOT_FOUND) {
+    console.error(MODULE_NOT_FOUND);
+    process.exit(1);
+}
+
+/*
+ * test module
+ */
+describe('autokey',function() {
+
+    var a;// key
+    var b;// data
+
+    it('string - should return some string',function(done) {
+
+        a = 'pippo';
+        b = 'ciao';
+        var cipher = autokey(a);
+        var d = cipher.encodeString(b); // encrypt
+        var e = cipher.decodeString(d); // decrypt
+        assert.deepEqual(b,e,'clear');
+        assert.notDeepEqual(b,d,'orig - encrypt');
+        assert.notDeepEqual(e,d,'encrypt - decrypt');
+        done();
+    });
+
+    it('array - should return some array',function(done) {
+
+        a = [112,105,112,112,111];
+        b = [99,105,97,111];
+        var cipher = autokey(a);
+        var d = cipher.encodeArray(b); // encrypt
+        var e = cipher.decodeArray(d); // decrypt
+        assert.deepEqual(b,e,'clear');
+        assert.notDeepEqual(b,d,'orig - encrypt');
+        assert.notDeepEqual(e,d,'encrypt - decrypt');
+        done();
+    });
+
+    it('buffer - should return some buffer',function(done) {
+
+        a = new Buffer('pippo');
+        b = new Buffer('ciao');
+        var cipher = autokey(a);
+        var d = cipher.encodeBuffer(b); // encrypt
+        var e = cipher.decodeBuffer(d); // decrypt
+        assert.deepEqual(b,e,'clear');
+        assert.notDeepEqual(b,d,'orig - encrypt');
+        assert.notDeepEqual(e,d,'encrypt - decrypt');
+        done();
+    });
+
+    it('wrong - change key',function(done) {
+
+        a = 'pippo';
+        b = 'ciao';
+        var cipher = autokey(a);
+        var d = cipher.encodeString(b); // encrypt
+        cipher.change('pluto'); // change key
+        var e = cipher.decodeString(d); // decrypt
+        assert.notDeepEqual(b,e,'clear');
+        assert.notDeepEqual(b,d,'orig - encrypt');
+        assert.notDeepEqual(e,d,'encrypt - decrypt');
+        done();
+    });
+
+});
