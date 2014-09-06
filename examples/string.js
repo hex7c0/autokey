02ab9fc06167e80753f787b7c6e85157b1f263fe
hex7c0 <carniellifrancesco@gmail.com>
2014-09-06 15:39:31 +0200

    input_encoding and output_encoding

--- examples/string.js
+++ examples/string.js
@@ -14,19 +14,19 @@
  */
 // import
 try {
-    var autokey = require('../index.min.js'); // use require('autokey') instead
+    var autokey = require('../index.min.js'); // use require('autokey')
 } catch (MODULE_NOT_FOUND) {
     console.error(MODULE_NOT_FOUND);
     process.exit(1);
 }
 
-var a = 'pi'; // key
+var a = 'pippo'; // key
 var b = 'ciao'; // data
 var cipher = autokey(a);
 
-var d = cipher.encodeString(b); // encrypt
+var d = cipher.encodeString(b, 'utf8', 'base64'); // encrypt
 
-var e = cipher.decodeString(d); // decrypt
+var e = cipher.decodeString(d, 'base64'); // decrypt
 
 console.log('original: ' + b);
 console.log('encrypt: ' + d);
