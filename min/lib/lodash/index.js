e5d76d0573a7d6a026d175182874c7dcaaea722f
hex7c0 <carniellifrancesco@gmail.com>
2014-10-04 16:43:50 +0200

    strict

--- min/lib/lodash/index.js
+++ min/lib/lodash/index.js
@@ -1,6 +1,1 @@
-/*
- * autokey v2.0.1
- * (c) hex7c0 http://supergiovane.tk/#/autokey
- * Licensed under GPLv3
- */
-"use strict";var min=__dirname+"/";module.exports=function(a){var b=require(min+"autokey.js");if(!a)throw new TypeError("password required");return b(a)};
+'use strict';var min=__dirname+"/";module.exports=function(a){var b=require(min+"autokey.js");if(!a)throw new TypeError("password required");return b(a)};
