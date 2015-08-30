bd7b43fc6e859a56444bb60cb62d71c9439a6cf1
hex7c0 <carniellifrancesco@gmail.com>
2015-08-30 21:16:49 +0200

    main: add functions shortcuts

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @file autokey main
  * @module autokey
  * @subpackage main
- * @version 2.0.0
+ * @version 2.2.0
  * @author hex7c0 <hex7c0@gmail.com>
  * @copyright hex7c0 2014
  * @license GPLv3
@@ -12,8 +12,9 @@
 /*
  * initialize module
  */
-// load
 var min = __dirname + '/min/lib/';
+var minNormal = min + 'normal/index.js';
+var minLodash = min + 'lodash/index.js';
 
 /*
  * functions
@@ -25,13 +26,41 @@ var min = __dirname + '/min/lib/';
  * @function autokey
  * @param {String|Array|Buffer} password - user key
  * @param {Boolean} [lodash] - flag
- * @return {Autokey}
+ * @return {Object}
  */
 function autokey(password, lodash) {
 
-  if (lodash) {
-    return require(min + 'lodash/index.js')(password);
+  if (!lodash) {
+    return require(minNormal)(password);
   }
-  return require(min + 'normal/index.js')(password);
+  return require(minLodash)(password);
 }
 module.exports = autokey;
+
+/**
+ * export normal function
+ * 
+ * @exports normal
+ * @function normal
+ * @param {String|Array|Buffer} password - user key
+ * @return {Object}
+ */
+function normal(password) {
+
+  return require(minNormal)(password);
+}
+module.exports.normal = normal;
+
+/**
+ * export normal function
+ * 
+ * @exports normal
+ * @function normal
+ * @param {String|Array|Buffer} password - user key
+ * @return {Object}
+ */
+function lodash(password) {
+
+  return require(minLodash)(password);
+}
+module.exports.lodash = lodash;
